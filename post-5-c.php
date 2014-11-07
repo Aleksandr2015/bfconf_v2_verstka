3996792b2cc9811a3c9e7384d88682def2130ddc
Aleksandr2015 <charleksvik@gmail.com>
2014-11-07 10:39:26 +0200

    перенаправление на другую страницу

---
+++ post-5-c.php
@@ -0,0 +1,15 @@
+<?php
+$name = $_POST['name'];
+$company = $_POST['company'];
+$tel = $_POST['tel'];
+$message = '<html><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><body>';
+$message .= '<h3>Имя</h3>'.$name.'<h3>Компания</h3>'.$company.'<h3>Телефон</h3>'.$tel;
+$message .= '</body></html>';
+$headers = "MIME-Version: 1.0"."\r\n";
+$headers .= "Content-Type: text/html; charset=utf-8"."\r\n"; // можно отправлять письмо в html
+$headers .= "From: bfconf.ru";
+mail('bfconf@gmail.com', 'Заявка обратного звонка по III международная конференция «Рынок оборудования и технологий для производства стали»', $message, $headers);
+//header("location: ".$_SERVER['HTTP_REFERER']);
+header("Location: after-send.html");
+exit;
+?>
